ba57e1171cb6e6faa8852a21fb061c80d26622f3
Ankit Kumar Sachan <ankitkumar.sachan@photon.com>
2023-04-18 20:26:35 +0530

    fixed build error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,25 @@
 import Head from "next/head";
+import { useEffect } from "react";
 import HeaderComponent from "@/components/header";
 import FooterComponent from "@/components/footer";
 import CarouselComponent from "@/components/carousel";
 import CategoryComponent from "@/components/category";
 import LearningSectionComponent from "@/components/learning_section/index1.js";
 import CookiesComponent from "@/components/cookies";
+import useAxios from "@/hooks/useApi";
 
 export default function Home() {
+  const { response, error, loading } = useAxios({
+    method: "GET",
+    url: "https://mcco02mstrub73kinte.dxcloud.episerver.net/api/episerver/v3.0/content?ContentUrl=https://mcco02mstrub73kinte.dxcloud.episerver.net/en/home/&expand=*",
+    headers: {
+      "Accept-Language": "en",
+    },
+  });
+
+  useEffect(() => {
+    console.log(response?.data[0]?.blockArea?.expandedValue, error, loading);
+  });
   return (
     <>
       <Head>
