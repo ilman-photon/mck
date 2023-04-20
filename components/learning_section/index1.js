bd6a9e010781f3d08e0515baa2af80e363386fb8
Rajaram Sathiyamoorthy (Photon) <rajaram.sathiyamoorthy@photon.in>
2023-04-20 15:20:28 +0530

    left and right align

--- components/learning_section/index1.js
+++ components/learning_section/index1.js
@@ -10,15 +10,15 @@ export default function LearningSectionComponent({ sectionData }) {
       {sectionData?.map((ele) => {
         return (
           <div
-            className="container mx-auto grid md:grid-cols-2 w-full"
+            className={`${(ele?.assetPosition?.value) === 'Right' ? 'flex-row-reverse' : ''} container mx-auto flex w-full`}
             key={ele?.image?.value?.id}
           >
             <div
-              className={`${ele?.assetPosition?.value} h-auto w-full p-5 lg:px-20 lg:my-10`}
+              className={`${ele?.assetPosition?.value} h-auto w-full p-5 lg:px-20 lg:my-10 col-span-1`}
             >
               <img src={ele?.image?.value?.url} />
             </div>
-            <div className="p-5 lg:p-16 my-auto">
+            <div className="p-5 lg:p-16 my-auto col-span-2">
               <h2 className="text-2xl lg:text-5xl text-gtl-med text-center lg:text-left blue-txt">
                 {ele?.name}
               </h2>
