8477e7ca0785022dfb80c60a7550db8685823e28
Pradeep Gunasekaran <pradeep.gunasekaran@photon.com>
2023-04-28 20:28:14 +0530

    Why F and T UI Styles

---
+++ components/why_FT/index.tsx
@@ -0,0 +1,58 @@
+function WhyFTComponent() {
+
+    return (
+        <>
+        {/* <div className="">
+                    <img src="images/why F and T header banner.png" />
+                </div> */}
+            <div className="four-oh-four container flex mx-auto px-0">
+                
+            <div className="heading pb-7 text-center">Quality Ingredients. Better Value.</div>
+                <div className="grey-txt pb-12 text-sofia-reg text-lg font-normal">
+                    <p className="pb-4">You hold your health in high regard, and you know that being well starts with being good to yourself. But with confusing treatments, expensive products, and options overload, managing your health can sometimes be a headache. Good news is around the corner.</p>
+
+                    <p className="pb-4">We’re Foster & Thrive, and we’re bringing a new take to your health journey. We combine clarity and care, so you can find what you need more easily, and with greater confidence.</p>
+
+                    <p>At Foster & Thrive, we believe the over-the-counter products should go beyond the box. And that wellness and being well go hand in hand. So we make our own products with high quality ingredients. We make it easy to find the products that meet your precise needs, and we stand behind you with intelligence and compassion at every turn of your health journey. At Foster & Thrive our goal is to help you beyond sickness and health, to the place where you prosper and thrive. </p>
+                </div>
+                <div className="relative mb-12">
+                    <img src="images/health_journey.png" />
+                    <div className="absolute top-0 left-0 bg-transparent-left py-55 pr-11 pl-6 w-540">
+                        <h2 className="text-mckblue text-gtl-med heading font-medium pb-6">We can help in your health journey.</h2>
+                        <p className="pb-6 text-sofia-reg text-lg font-normal text-mcknormalgrey">We combine the quality of marketing-leading products and the legacy of a health leader. Our expertise lends to and unwavering confidence of knowing what to do and how to do it, with reason, clarity, and consistency. </p>
+                        <button className="p-3 uppercase rounded-lg blue-bg text-white text-sofia-bold font-extrabold text-base float-right">View Products</button>
+                    </div>
+                </div>
+                <div className="mb-12">
+                    <img src="images/With you for life image.png" />
+                </div>
+                <div className="relative mb-12">
+                    <img src="images/heal and thrive.png" />
+                    <div className="absolute top-0 right-0 bg-transparent-right py-55 pr-11 pl-6 w-540 float-right">
+                        <h2 className="text-mckblue text-gtl-med heading font-medium pb-6">You need products you can trust to heal and thrive.</h2>
+                        <p className="pb-6 text-sofia-reg text-lg font-normal text-mcknormalgrey">Our products are designed to meet you at every turn of your health care journey, wherever that may lead.</p>
+                        <button className="p-3 uppercase rounded-lg blue-bg text-white text-sofia-bold font-extrabold text-base float-right">View Products</button>
+                    </div>
+                </div>
+                <div className="w-full grid lg:flex items-center mb-12">
+                    <div className="w-3/6 pl-18 pr-12">
+                        <h2 className="text-mckblue text-gtl-med heading font-medium pb-6">You need products you can trust to heal and thrive.</h2>
+                        <p className="pb-6 text-sofia-reg text-lg font-normal text-mcknormalgrey">Our products are designed to meet you at every turn of your health care journey, wherever that may lead.</p>
+                    </div>
+                    <div className="w-3/6 rounded-lg overflow-hidden"><img src="images/My headaches.png" /></div>
+                </div>
+
+                <div className="w-full grid lg:flex items-center">
+                    <div className="w-3/6 rounded-lg overflow-hidden"><img src="images/My headaches.png" /></div>
+                    <div className="w-3/6 pl-18 pr-12">
+                        <h2 className="text-mckblue text-gtl-med heading font-medium pb-6">You need products you can trust to heal and thrive.</h2>
+                        <p className="pb-6 text-sofia-reg text-lg font-normal text-mcknormalgrey">Our products are designed to meet you at every turn of your health care journey, wherever that may lead.</p>
+                        <button className="p-3 uppercase rounded-lg blue-bg text-white text-sofia-bold font-extrabold text-base float-right">View Products</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+
+}
+export default WhyFTComponent;
