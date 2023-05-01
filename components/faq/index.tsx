e42cc156286856c8555e472bc5f50a8d90d6d92f
Rajaram Sathiyamoorthy (Photon) <rajaram.sathiyamoorthy@photon.in>
2023-05-02 01:48:26 +0530

    footer integration

--- components/faq/index.tsx
+++ components/faq/index.tsx
@@ -1,10 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function FaqComponent() {
+
+    const [faqData, setFaqData] = useState<any>();
+
+    const getFaqData = async () => {
+        const response = await axios.get(            
+            `${process.env.API_URL}/api/episerver/v3.0/content?ContentUrl=${process.env.API_URL}/en/generic/faq/&expand=*`
+        );
+        setFaqData(response);
+    };
+
+    useEffect(() => {
+        getFaqData();
+    }, []);
+
+    console.log(faqData, "faq");
+
+    //console.log(faqData.data[0]);
+
     return (
         <>
             <div className="four-oh-four container flex mx-auto px-0">
                 <div className="heading pb-3 text-center">FAQ</div>
-                <div className="text-center text-mckblue text-gtl-med pb-6">Have questions? We here to help.</div>
-                
+                <div className="text-center text-mckblue text-gtl-med pb-6"></div>
+
                 <div className="tab text-mckblue text-sofia-bold font-extrabold text-lg w-full overflow-hidden rounded-lg mb-6">
                     <input type="checkbox" id="chck1" />
                     <label className="tab-label shade-blue-bg py-3 px-4 flex justify-between cursor-pointer" htmlFor="chck1">What types of events are eligible for grants?</label>
@@ -33,8 +54,8 @@ function FaqComponent() {
                 <div className="shade-blue-border w-full mt-6 rounded-lg overflow-hidden">
                     <div className="shade-blue-bg text-mckblue text-sofia-bold font-extrabold text-lg py-3 px-4">Have a different question?</div>
                     <div className="shade-blue-border-top text-gtl-med font-medium text-base text-mckblue p-6 text-27">
-                    <p className="pb-4">Not finding the answers to your questions?</p>
-                    <button className="rounded-lg blue-bg text-white p-3 text-sofia-bold font-extrabold text-base uppercase">contact us</button>
+                        <p className="pb-4">Not finding the answers to your questions?</p>
+                        <button className="rounded-lg blue-bg text-white p-3 text-sofia-bold font-extrabold text-base uppercase">contact us</button>
                     </div>
                 </div>
 
@@ -48,18 +69,18 @@ function FaqComponent() {
                         </div>
                     </div>
                     <div className="sm:mt-6 lg:mt-0">
-                    <div className="rounded-lg shade-blue-border overflow-hidden">
-                        <div className="shade-blue-bg text-mckblue text-sofia-bold font-extrabold text-lg py-3 px-4">Speak to a pharmacist</div>
-                        <div className="p-6 text-mckblue shade-blue-border-top text-lg text-sofia-reg">
-                            <a href="" className="underline block pb-1">Find a store</a>
+                        <div className="rounded-lg shade-blue-border overflow-hidden">
+                            <div className="shade-blue-bg text-mckblue text-sofia-bold font-extrabold text-lg py-3 px-4">Speak to a pharmacist</div>
+                            <div className="p-6 text-mckblue shade-blue-border-top text-lg text-sofia-reg">
+                                <a href="" className="underline block pb-1">Find a store</a>
+                            </div>
                         </div>
-                    </div>
-                    <div className="rounded-lg shade-blue-border overflow-hidden mt-6">
-                        <div className="shade-blue-bg text-mckblue text-sofia-bold font-extrabold text-lg py-3 px-4">Send Feedback</div>
-                        <div className="p-6 text-mckblue shade-blue-border-top text-lg text-sofia-reg">
-                            <p>Help us make our search better Send <a href="" className="underline">feedback</a></p>
+                        <div className="rounded-lg shade-blue-border overflow-hidden mt-6">
+                            <div className="shade-blue-bg text-mckblue text-sofia-bold font-extrabold text-lg py-3 px-4">Send Feedback</div>
+                            <div className="p-6 text-mckblue shade-blue-border-top text-lg text-sofia-reg">
+                                <p>Help us make our search better Send <a href="" className="underline">feedback</a></p>
+                            </div>
                         </div>
-                    </div>
                     </div>
                 </div>
             </div>
--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -6,26 +6,30 @@ import { useState, useEffect } from "react";
 export default function FooterComponent() {
 
   const [footerData, setFooterData] = useState<any>();
+  const [footerSecondData, setFooterSecondData] = useState<any>();
 
   const getData = async () => {
+
     const response = await axios.get(
-      `${process.env.API_URL}/api/episerver/v3.0/content/?ContentUrl=${process.env.API_URL}/en/application-settings/&expand=*`
-    );
-    const secondBlock = response.data[0].footer.expandedValue[0].contentLink.id;
-    const responseid = await axios.get(
-      `https://mcco02mstrub73kinte.dxcloud.episerver.net/api/episerver/v3.0/content/?References=268&expand=*`,{headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json'}},
-     // `${process.env.API_URL}/api/episerver/v3.0/content/?References=${secondBlock}&expand=*`
+      //`https://mcco02mstrub73kinte.dxcloud.episerver.net/api/episerver/v3.0/content/?References=268&expand=*`,{headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json', 'Accept-Language': 'en'}},
+      `${process.env.API_URL}/api/episerver/v3.0/content/?ContentUrl=${process.env.API_URL}/en/application-settings/&expand=*`, { headers: { 'Accept-Language': 'en' } },
     );
 
-    const red = `${process.env.API_URL}/api/episerver/v3.0/content/?References=${secondBlock}&expand=*`;
-    setFooterData(responseid);
-    console.log(red, "1");
-    console.log(responseid,"3")
+    const secondBlock = response.data[0].footer.expandedValue[0].contentLink.id;
+    const responseid = await axios.get(
+      `${process.env.API_URL}/api/episerver/v3.0/content/?References=${secondBlock}&expand=*`, { headers: { 'Accept-Language': 'en' } },
+    );
+    setFooterData(response);
+    setFooterSecondData(responseid);
+    console.log(responseid, "2")
   };
-  console.log(footerData, "2");
+  //console.log(footerData, "3");
   useEffect(() => {
     getData();
   }, []);
+  
+  //console.log(footerSecondData, "1")
+ // console.log(footerSecondData?.data[0]?.socialMediaLinkBlock?.expandedValue[0], "final");
 
 
   return (
@@ -34,136 +38,40 @@ export default function FooterComponent() {
         <div className="grid md:grid-cols-3 lg:grid-cols-3">
           <div className=" my-6 lg:my-0 text-gtl-med">
             <ul className="mb-0 list-none lg:py-0 px-0">
-              <li className="my-2 lg:my-0">
-                <Link
-                  className="text-sofia-reg text-lg text-mcknormalgrey"
-                  rel="stylesheet"
-                  href="/contact_us"
-                >
-                  Contact Us{" "}
-                </Link>
+              <li className="my-2 lg:my-0 grid">
+                {footerData?.data[0]?.footer?.expandedValue[0]?.menuItemsColumn1?.expandedValue.map((link:any)=>(
+                   <Link
+                   className="text-sofia-reg text-lg text-mcknormalgrey my-2"
+                   rel="stylesheet"
+                   href={link?.menuUrl?.value}
+                 >{link?.menuTitle?.value}
+                 </Link>
+                ))}               
               </li>
-              <li className="my-2">
-                <Link
-                  className="text-sofia-reg text-lg text-mcknormalgrey"
-                  href="/faq"
-                >
-                  FAQ
-                </Link>
-              </li>
-              <li className="my-2">
-                <Link
-                  href="#!"
-                  type="button"
-                  className="mr-3 ml-0 h-6 w-6 icon-bg rounded-full leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="mx-auto h-full w-4"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
-                  </svg>
-                </Link>
-                <Link
-                  href="#!"
-                  type="button"
-                  className="ml-0 h-6 w-6 icon-bg rounded-full leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="mx-auto h-full w-4"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-                  </svg>
-                </Link>
-                <Link
-                  href="#!"
-                  type="button"
-                  className="ml-3 h-6 w-6 icon-bg rounded-full leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="mx-auto h-full w-4"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
-                  </svg>
-                </Link>
-                <Link
-                  href="#!"
-                  type="button"
-                  className="ml-3 h-6 w-6 icon-bg rounded-full leading-normal text-white transition duration-150 ease-in-out hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0"
-                  data-te-ripple-init
-                  data-te-ripple-color="light"
-                >
-                  <svg
-                    width="24"
-                    height="25"
-                    viewBox="0 0 24 25"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <rect
-                      y="0.75"
-                      width="24"
-                      height="24"
-                      rx="12"
-                      fill="#001A71"
-                    />
-                    <path
-                      d="M19.8406 9.54922C19.8406 9.54922 19.6844 8.44609 19.2031 7.96172C18.5938 7.32422 17.9125 7.32109 17.6 7.28359C15.3625 7.12109 12.0031 7.12109 12.0031 7.12109H11.9969C11.9969 7.12109 8.6375 7.12109 6.4 7.28359C6.0875 7.32109 5.40625 7.32422 4.79688 7.96172C4.31563 8.44609 4.1625 9.54922 4.1625 9.54922C4.1625 9.54922 4 10.8461 4 12.1398V13.3523C4 14.6461 4.15937 15.943 4.15937 15.943C4.15937 15.943 4.31562 17.0461 4.79375 17.5305C5.40313 18.168 6.20313 18.1461 6.55938 18.2148C7.84063 18.3367 12 18.3742 12 18.3742C12 18.3742 15.3625 18.368 17.6 18.2086C17.9125 18.1711 18.5938 18.168 19.2031 17.5305C19.6844 17.0461 19.8406 15.943 19.8406 15.943C19.8406 15.943 20 14.6492 20 13.3523V12.1398C20 10.8461 19.8406 9.54922 19.8406 9.54922ZM10.3469 14.8242V10.3273L14.6687 12.5836L10.3469 14.8242Z"
-                      fill="white"
-                    />
-                  </svg>
-                </Link>
+              <li className="my-2 flex">
+              {footerSecondData?.data[0]?.socialMediaLinkBlock?.expandedValue.map((sociallink:any)=>(
+                   <Link
+                   className="text-sofia-reg text-lg text-mcknormalgrey"
+                   rel="stylesheet"
+                   href={sociallink?.socialMediaImage?.expandedValue?.url}
+                 ><img src={sociallink?.socialMediaImage?.expandedValue?.thumbnail?.value?.url} alt={sociallink?.socialMediaImage?.expandedValue?.altText?.value}></img>
+                 </Link>
+                ))} 
               </li>
             </ul>
           </div>
 
           <div className="f-border py-6 lg:py-0 lg:pl-6 pl-0">
             <ul className="mb-0 list-none">
-              <li className="my-2 lg:my-0">
-                <Link
-                  className="text-sofia-reg text-lg text-mcknormalgrey"
-                  href="#!"
-                >
-                  Privacy Notice
-                </Link>
-              </li>
-              <li className="my-2">
-                <Link
-                  className="text-sofia-reg text-lg text-mcknormalgrey"
-                  href="#!"
-                >
-                  Terms of Use
-                </Link>
-              </li>
-              <li className="my-2">
-                <Link
-                  className="text-sofia-reg text-lg text-mcknormalgrey"
-                  href="#!"
-                >
-                  Cookie Settings
-                </Link>
-              </li>
-              <li className="my-2">
-                <Link
-                  className="text-sofia-reg text-lg text-mcknormalgrey"
-                  href="#!"
-                >
-                  Do Not Sell My Information
-                </Link>
+              <li className="my-2 lg:my-0 grid">
+              {footerSecondData?.data[0]?.menuItemsColumn2?.expandedValue.map((slink:any)=>(
+                   <Link
+                   className="text-sofia-reg text-lg text-mcknormalgrey py-2"
+                   rel="stylesheet"
+                   href={slink?.menuUrl?.value}
+                 >{slink?.menuTitle?.value}
+                 </Link>
+                ))}
               </li>
             </ul>
           </div>
@@ -171,14 +79,16 @@ export default function FooterComponent() {
           <div id="footer-sign-up" className="px-0 py-0 pl-0 lg:pl-6">
             <div className="px-3 py-3 lg:px-3 bg-white rounded-lg border-mcknormalgrey border-solid border mt-5 lg:mt-0">
               <h5 className="m-0 pb-3 px-0 text-2xl text-gtl-med text-mckblue">
-                Email Signup & Rewards
+              {footerSecondData?.data[0]?.subscription?.expandedValue[0]?.name}
               </h5>
-              <div className="px-0 text-lg text-sofia-reg text-mcknormalgrey">
-                Want to receive tips, personalized content and exclusive offers
-                from Foster & Thrive?
+              <div className="px-0 text-lg text-sofia-reg text-mcknormalgrey" 
+              dangerouslySetInnerHTML={{
+                __html: footerSecondData?.data[0]?.subscription?.expandedValue[0]?.description.value,
+              }}
+              >
               </div>
               <div className="py-3 px-0 text-lg text-sofia-reg text-mcknormalgrey">
-                Sign up today!
+              {footerSecondData?.data[0]?.subscription?.expandedValue[0]?.subTitle?.value}
               </div>
               <SignUpComponent />
             </div>
