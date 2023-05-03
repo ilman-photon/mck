14d804119617d35f30c6987a4214a000b7183400
Rajaram Sathiyamoorthy (Photon) <rajaram.sathiyamoorthy@photon.in>
2023-05-03 21:57:41 +0530

    pdp changes

--- components/pdp_carousel/index.tsx
+++ components/pdp_carousel/index.tsx
@@ -5,7 +5,7 @@ function PdpCarousel(prodViewData: any) {
                 <div className="">
                     <div className="_2mLllQ">
                         <ul className="_3GnUWp">
-                            {prodViewData.value[0].productImages.value.slice(0, 5).map((imgdata: any) => (
+                            {prodViewData?.value[0]?.productImages?.value?.slice(0, 5).map((imgdata: any) => (
                                 <li className="w-24 h-24 rounded box-border flex flex-row justify-center items-center p-2 bg-white border border-solid border-mckblue mb-3" key={imgdata?.id}>
                                     <img className="max-w-xl w-10" src={imgdata?.url}  />
                                 </li>
@@ -15,7 +15,7 @@ function PdpCarousel(prodViewData: any) {
                     </div>
                 </div>
                 <div className="w-[526px] h-[636px] box-border flex flex-row justify-center items-center p-2 bg-white rounded border border-solid border-mckblue ml-14">
-                    <img alt="" src={prodViewData.value[0].productImages.value[1].url} />
+                    <img alt="" src={prodViewData?.value[0]?.productImages?.value[1]?.url} />
                 </div>
             </div>
         </div>
