512dc9b149a8c995cb1089d1e92443ab390cec37
Ramkumar Subramaniam <ramkumar.subramaniam@photon.com>
2023-05-05 19:12:40 +0530

    api integrated

--- components/503/503.tsx
+++ components/503/503.tsx
@@ -1,28 +1,41 @@
-function FourOhFourCoumponent() {
+import axios, { AxiosError } from "axios";
+import { useState, useEffect } from "react";
+
+function ServiceUnavailableCoumponent() {
+    const [serviceErrorData, SetServiceErrorData] = useState<any>();
+
+    function fetchServiceUnavailableDetails() {
+        return axios.get(
+            `${process.env.API_URL}/api/episerver/v3.0/content?ContentUrl=${process.env.API_URL}/en/generic/server-error/&expand=*`,
+            {
+                headers: {
+                    "Accept-Language": "en",
+                },
+            }
+        );
+    }
+
+    useEffect(() => {
+        fetchServiceUnavailableDetails()
+            .then((res) => {
+                console.log(res)
+                SetServiceErrorData(res);
+            })
+            .catch((e: Error | AxiosError) => console.log(e));
+    }, []);
+
     return (
         <>
             <div className="four-oh-four container flex flex-row mx-auto">
-                <div className="width-416">
-                <div className="lg:heading pb-3 text-32 text-gtl-med font-medium" id="err-404-label-001">Oops! </div>
-                <div className="lg:text pb-6 font-medium text-2xl text-mckblue text-gtl-med" id="err-404-label-002">We can’t find the page you are looking for.</div>
-                <div className="lg:hint pb-6 text-xl">Not to worry...</div>
-                <div className="table-container">
-                    <div className="table-heading font-extrabold text-sofia-bold" id="err-404-label-003">Here’s what you can do.</div>
-                    <div className="normal-text p-6 text-sofia-reg font-normal lg:text-lg text-base">
-                    <p id="err-404-label-004" className="lg:text-lg text-base lg:pb-1 pb-2 text-sofia-reg font-normal">Retype the URL</p>
-                    <a href="#" className="text-mckblue block lg:pb-1 pb-2">Home</a>
-                    <a href="#" className="text-mckblue block lg:pb-1 pb-2">Sitemap</a>
-                    <a href="#" className="text-mckblue block lg:pb-1 pb-2">FAQ</a>
-                    <a href="#" className="text-mckblue block">Send Feedback</a>
-                    </div>
+                <div className="lg:width-416 w-full">
+                <div className="lg:heading pb-3 text-32 text-gtl-med font-medium text-mckblue" id="err-404-label-001">{serviceErrorData?.data[0]?.title.value}</div>
+                <div className="lg:text pb-6 font-medium text-2xl text-mckblue text-gtl-med" id="err-404-label-002" dangerouslySetInnerHTML={{ __html: serviceErrorData?.data[0]?.description.value}}></div>
                 </div>
-                </div>
-                
-                <div className="bg-logo-right overflow-hidden lg:relative absolute"><img src="images/Oops.svg" alt="oops" id="err-404-img-005"/></div>
+                <div className="bg-logo-right overflow-hidden lg:relative absolute"><img src={serviceErrorData?.data[0]?.backgroundImage?.expandedValue.url} alt="oops" id="err-404-img-005"/></div>
             </div>
             
         </>
     );
 }
 
-export default FourOhFourCoumponent;
+export default ServiceUnavailableCoumponent;
