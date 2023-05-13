adf89d9d7df1c6f820c439ce58979a107e6fe15a
bharath <bharath.r1@photon.in>
2023-05-13 23:42:49 +0530

    Product_listing_page

--- pages/all_product_category/index.tsx
+++ pages/all_product_category/index.tsx
@@ -244,6 +244,41 @@ function AllProductCategoryPage() {
       {!categoryLoading && !categoryError && categoryData && (
         <CategoryComponent sectionData={[categoryData]} />
       )}
+
+      {/* Category Four col Start */}
+      
+      {/* <div className="container w-full mx-auto my-6 mb-0 lg:my-20 grid grid-cols-2 gap-4 lg:grid-cols-4 px-4 lg:px-0">
+      {loading && <p>Loading...</p>}
+
+      {!loading &&
+        response &&
+        response.map((item) => {
+          return (
+            <div id="category" key={item?.data?.contentLink?.id} className="mb-6">
+              <div className="mx-auto w-36 lg:w-52 h-36 lg:h-52">
+                <style jsx>{`
+                  .border {
+                    border: 2px solid ${item?.data?.backgroundColor?.value};
+                  }
+                `}</style>
+                <img src={item?.data?.productCategoryImage?.value?.url} alt="category image" id={item?.data?.productCategoryImage?.value?.url} tabIndex={0}/>
+              </div>
+              <div className="text-center text-gtl-med text-xl lg:text-2xl mt-6 lg:mt-10 text-mckblue" tabIndex={0} id={item?.data?.name}>
+                {item?.data?.name}
+              </div>
+              <div
+                className="text-center text-sofia-reg font-normal w-3/4 mx-auto text-base lg:text-lg text-mcknormalgrey"
+                dangerouslySetInnerHTML={{
+                  __html: item?.data?.productCategoryDescription?.value,
+                }} tabIndex={0} id={item?.data?.productCategoryDescription?.value}></div>
+            </div>
+          );
+        })}
+    </div> */}
+
+      {/* Category Four col End */}
+      
+
       <div className="container lg:p-18">
         {/* <div>Active Filter</div>
         <div>Showing 65 results</div> */}
@@ -804,6 +839,7 @@ function AllProductCategoryPage() {
           </div>
         </div>
       </div>
+
       <FooterComponent />
     </>
   );
