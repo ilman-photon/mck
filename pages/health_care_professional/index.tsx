69afa26d67ce2ecd3a6847f5d7e9eb7af5a9e117
Pradeep Gunasekaran <pradeep.gunasekaran@photon.com>
2023-05-13 15:57:50 +0530

    873 Health care professional UI

---
+++ pages/health_care_professional/index.tsx
@@ -0,0 +1,17 @@
+import FooterComponent from "@/components/footer";
+import HealthCareProfessionalComponent from "@/components/health_care_professional";
+// import HeaderNoStickyComponent from "@/components/header_nosticky";
+import HeaderComponent from "@/components/header";
+import React from "react";
+
+function Terms() {
+    return (
+        <>
+        <HeaderComponent />
+        <HealthCareProfessionalComponent />
+        <FooterComponent />
+    </>
+    )
+    
+}
+export default Terms;
