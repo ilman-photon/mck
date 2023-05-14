6a4b48ca21cf90272722bdfabf64896463d7faf1
Rajaram Sathiyamoorthy (Photon) <rajaram.sathiyamoorthy@photon.in>
2023-05-14 22:43:21 +0530

    blog serach result page

---
+++ pages/blog-search-result/index.tsx
@@ -0,0 +1,19 @@
+import FooterComponent from "@/components/footer";
+//import HeaderComponent from "@/components/header";
+import React from "react";
+import GoogleTagManager from "@/components/google_tag_manager";
+import HeaderComponent from "@/components/header";
+import BlogSearchComponent from "@/components/blog/blog-search-result";
+
+function Faq() {
+    return (
+        <>
+        <GoogleTagManager/>
+        <HeaderComponent />
+        <BlogSearchComponent/>
+        <FooterComponent />
+    </>
+    )
+    
+}
+export default Faq;
