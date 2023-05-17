3d6f98028527d21aa4d845b40db17f009aa0c7d7
tamilvanan.b <tamilvanan.b@photon.com>
2023-05-17 12:42:45 +0000

    Revert "Merged PR 263: 1020 Fixes
    1020 Fixes"

--- components/contactus/ContactUsComponent.tsx
+++ components/contactus/ContactUsComponent.tsx
@@ -27,37 +27,37 @@ function ContactUsComponent() {
 
     return (
         <>
-            <div className="four-oh-four contact-us container flex flex-col mx-auto px-0 lg:mt-36 mt-16">
-                <div className="heading pb-6" id="cu_label_001" tabIndex={0} aria-label={contactUsData?.data[0]?.title.value}>{contactUsData?.data[0]?.title.value}</div>
+            <div className="four-oh-four container flex flex-col mx-auto px-0 lg:mt-36 mt-16">
+                <div className="heading pb-6" id="cu_label_001">{contactUsData?.data[0]?.title.value}</div>
                 <div className="grid lg:grid-cols-2 lg:gap-4">
                     <div className="overflow-hidden rounded-lg text-mckthingrey border contact-container lg:mb-12 mb-6">
-                        <div className="text-mckblue lg:py-3 lg:px-4 p-3 lg:text-lg text-base font-extrabold shade-blue-bg text-sofia-bold" id="cu_label_002" tabIndex={0} aria-label={contactUsData?.data[0]?.contentArea.expandedValue[0].contactInformationHeading.value}>{contactUsData?.data[0]?.contentArea.expandedValue[0].contactInformationHeading.value}</div>
+                        <div className="text-mckblue lg:py-3 lg:px-4 p-3 lg:text-lg text-base font-extrabold shade-blue-bg text-sofia-bold" id="cu_label_002">{contactUsData?.data[0]?.contentArea.expandedValue[0].contactInformationHeading.value}</div>
                         <div className="lg:p-6 p-4 contact-body">
-                            <p className="text-base font-normal text-mcknormalgrey pb-3 inquiry-text text-sofia-reg" id="cu_label_003" dangerouslySetInnerHTML={{ __html: contactUsData?.data[0]?.contentArea.expandedValue[0].contactInformationDetail.value, }} tabIndex={0} aria-label={contactUsData?.data[0]?.contentArea.expandedValue[0].contactInformationDetail.value}></p>
+                            <p className="text-base font-normal text-mcknormalgrey pb-3 inquiry-text text-sofia-reg" id="cu_label_003" dangerouslySetInnerHTML={{ __html: contactUsData?.data[0]?.contentArea.expandedValue[0].contactInformationDetail.value, }}></p>
                             <div className="flex flex-col lg:flex-row lg:pb-3 pb-4">
                                 <span className="contact-number font-extrabold lg:text-lg text-base text-mckblue text-sofia-bold lg:mr-3 flex mb-1 lg:mb-0" id="cu_label_004">
-                                    <img src={contactUsData?.data[0]?.contentArea.expandedValue[0].phoneImage.expandedValue.url} alt={contactUsData?.data[0]?.contentArea.expandedValue[0].phoneImage.expandedValue.url} id="cu_img_005" tabIndex={0} aria-label={contactUsData?.data[0]?.contentArea.expandedValue[0].phoneNumber.value}/> {contactUsData?.data[0]?.contentArea.expandedValue[0].phoneNumber.value}</span>
-                                <p className="lg:text-base text-sm font-normal text-mcknormalgrey text-sofia-reg contact-number-desc" id="cu_label_006" tabIndex={0} aria-label={contactUsData?.data[0]?.contentArea.expandedValue[0].phoneTitle.value}>{contactUsData?.data[0]?.contentArea.expandedValue[0].phoneTitle.value}</p>
+                                    <img src={contactUsData?.data[0]?.contentArea.expandedValue[0].phoneImage.expandedValue.url} alt="Cell Icon" id="cu_img_005" /> {contactUsData?.data[0]?.contentArea.expandedValue[0].phoneNumber.value}</span>
+                                <p className="lg:text-base text-sm font-normal text-mcknormalgrey text-sofia-reg contact-number-desc" id="cu_label_006">{contactUsData?.data[0]?.contentArea.expandedValue[0].phoneTitle.value}</p>
                             </div>
                             <div className="flex flex-col lg:flex-row lg:pb-3 pb-4">
                                 <span className="contact-number font-extrabold lg:text-lg text-base text-mckblue text-sofia-bold lg:mr-3 flex mb-1 lg:mb-0" id="cu_label_007">
-                                    <img src={contactUsData?.data[0]?.contentArea.expandedValue[0].operationHourImage.expandedValue.url} alt={contactUsData?.data[0]?.contentArea.expandedValue[0].operationHourImage.expandedValue.url} id="cu_img_008" tabIndex={0} aria-label={contactUsData?.data[0]?.contentArea.expandedValue[0].operationHourTitle.value} />{contactUsData?.data[0]?.contentArea.expandedValue[0].operationHourTitle.value}</span>
-                                <p className="lg:text-base text-sm font-normal text-mcknormalgrey text-sofia-reg contact-number-desc" id="cu_label_009" tabIndex={0} aria-label={contactUsData?.data[0]?.contentArea.expandedValue[0].operationHourValue.value}>{contactUsData?.data[0]?.contentArea.expandedValue[0].operationHourValue.value}</p>
+                                    <img src={contactUsData?.data[0]?.contentArea.expandedValue[0].operationHourImage.expandedValue.url} alt="timer icon" id="cu_img_008" />{contactUsData?.data[0]?.contentArea.expandedValue[0].operationHourTitle.value}</span>
+                                <p className="lg:text-base text-sm font-normal text-mcknormalgrey text-sofia-reg contact-number-desc" id="cu_label_009">{contactUsData?.data[0]?.contentArea.expandedValue[0].operationHourValue.value}</p>
                             </div>
                             <div className="flex flex-col lg:flex-row lg:pb-3 pb-4">
                                 <span className="contact-number font-extrabold lg:text-lg text-base text-mckblue text-sofia-bold lg:mr-3 flex" id="cu_label_010">
-                                    <img src={contactUsData?.data[0]?.contentArea.expandedValue[0].emailImage.expandedValue.url} alt={contactUsData?.data[0]?.contentArea.expandedValue[0].emailImage.expandedValue.url} id="cu_img_011" tabIndex={0} aria-label={contactUsData?.data[0]?.contentArea.expandedValue[0].emailTitle.value} />{contactUsData?.data[0]?.contentArea.expandedValue[0].emailTitle.value}</span>
+                                    <img src={contactUsData?.data[0]?.contentArea.expandedValue[0].emailImage.expandedValue.url} alt="email icon" id="cu_img_011" />{contactUsData?.data[0]?.contentArea.expandedValue[0].emailTitle.value}</span>
                             </div>
                             <div className="flex flex-col lg:flex-row">
                                 <span className="contact-number font-extrabold lg:text-lg text-base lg:mr-3 text-mckblue text-sofia-bold flex mb-1 lg:mb-0" id="cu_label_012">
-                                    <img src={contactUsData?.data[0]?.contentArea.expandedValue[0].locationIcon.expandedValue.url} alt={contactUsData?.data[0]?.contentArea.expandedValue[0].locationIcon.expandedValue.url} id="cu_img_013" tabIndex={0} aria-label={contactUsData?.data[0]?.contentArea.expandedValue[0].locationTitle.value}/>{contactUsData?.data[0]?.contentArea.expandedValue[0].locationTitle.value}</span>
-                                <p className="lg:text-base text-sm font-normal text-mcknormalgrey text-sofia-reg contact-number-desc" id="cu_label_014" tabIndex={0} aria-label={contactUsData?.data[0]?.contentArea.expandedValue[0].locationValue.value}>{contactUsData?.data[0]?.contentArea.expandedValue[0].locationValue.value}</p>
+                                    <img src={contactUsData?.data[0]?.contentArea.expandedValue[0].locationIcon.expandedValue.url} alt="location icon" id="cu_img_013"/>{contactUsData?.data[0]?.contentArea.expandedValue[0].locationTitle.value}</span>
+                                <p className="lg:text-base text-sm font-normal text-mcknormalgrey text-sofia-reg contact-number-desc" id="cu_label_014">{contactUsData?.data[0]?.contentArea.expandedValue[0].locationValue.value}</p>
                             </div>
                         </div>
                     </div>
                     <div className="overflow-hidden rounded-lg text-mckthingrey border contact-container lg:mb-12 mb-6">
-                        <div className="text-mckblue lg:py-3 lg:px-4 p-3 lg:text-lg text-base font-extrabold shade-blue-bg text-sofia-bold" id="cu_label_015" tabIndex={0} aria-label={contactUsData?.data[0]?.contentArea.expandedValue[0].whatWeCandoHeading.value}>{contactUsData?.data[0]?.contentArea.expandedValue[0].whatWeCandoHeading.value}</div>
-                        <div className="lg:p-6 p-4 contact-body underline lg:text-lg text-base font-normal text-mckblue text-sofia-reg" id="cu_link_016" dangerouslySetInnerHTML={{ __html: contactUsData?.data[0]?.contentArea.expandedValue[0].whatWeCandoDetail.value, }} tabIndex={0} aria-label={contactUsData?.data[0]?.contentArea.expandedValue[0].whatWeCandoDetail.value}>
+                        <div className="text-mckblue lg:py-3 lg:px-4 p-3 lg:text-lg text-base font-extrabold shade-blue-bg text-sofia-bold" id="cu_label_015">{contactUsData?.data[0]?.contentArea.expandedValue[0].whatWeCandoHeading.value}</div>
+                        <div className="lg:p-6 p-4 contact-body underline lg:text-lg text-base font-normal text-mckblue text-sofia-reg" id="cu_link_016" dangerouslySetInnerHTML={{ __html: contactUsData?.data[0]?.contentArea.expandedValue[0].whatWeCandoDetail.value, }}>
                             {/* TODO Coordinate with CMS team to fix this */}
                             {/* <a href="" className="block pb-1 underline text-lg font-normal text-mckblue text-sofia-reg">Home</a> */}
                             {/* TODO Coordinate with CMS team to fix this */}
@@ -69,7 +69,7 @@ function ContactUsComponent() {
                         </div>
                     </div>
                 </div>
-                <p className="text-sofia-reg text-mcknormalgrey font-normal lg:text-base text-sm lg:pb-12" id="cu_label_017" dangerouslySetInnerHTML={{ __html: contactUsData?.data[0]?.contentArea.expandedValue[0].disclaimer.value, }} tabIndex={0} aria-label={contactUsData?.data[0]?.contentArea.expandedValue[0].disclaimer.value}></p>
+                <p className="text-sofia-reg text-mcknormalgrey font-normal lg:text-base text-sm lg:pb-12" id="cu_label_017" dangerouslySetInnerHTML={{ __html: contactUsData?.data[0]?.contentArea.expandedValue[0].disclaimer.value, }}></p>
             </div>
         </>
     );
