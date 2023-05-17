45a29d2c79b3ce8d95f453708510118b1b324f5b
tamilvanan.b <tamilvanan.b@photon.com>
2023-05-17 08:29:55 +0000

    Revert "Merged PR 273: Revert 'testcase for where to buy and contact page'
    Reverts !255"

--- __tests__/components/product_list.test.js
+++ __tests__/components/product_list.test.js
@@ -12,16 +12,17 @@ jest.mock('next/router', () => {
 
 describe('ProductListComponent - Display values from CMS', () => {
   beforeEach(() => {
-    axios.get.mockResolvedValue( {data: productFilter} );
-    axios.get.mockResolvedValue( {data: recommendedProductListData} );
-    axios.get.mockResolvedValue( {data: productCategoryData} );
-    axios.get.mockResolvedValue( {data: productListData} );
+    axios.get
+    .mockResolvedValue( {data: productFilter} )
+    .mockResolvedValue( {data: recommendedProductListData} )
+    .mockResolvedValue( {data: productCategoryData} )
+    .mockResolvedValue( {data: productListData} );
   });
 
   test('Render product list component', async () => {
     await act(async () => {
       render(<ProductListComponent />);
     });
-    expect(screen.getByText('Acute Care')).toBeInTheDocument();
+    expect(screen.getAllByText('You’re in touch with your health, able to pinpoint what you need. With Foster & Thrive, you can easily target and treat your precise needs.')).toBeTruthy();
   });
 });
