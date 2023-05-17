cbe3ff396b01a4f7484a3551bdd1b140c2ce2592
Ankit Kumar Sachan <ankitkumar.sachan@photon.com>
2023-05-17 16:50:33 +0530

    added footer links

---
+++ pages/do-not-sell-my-information/index.tsx
@@ -0,0 +1,14 @@
+import FooterComponent from "@/components/footer";
+import HeaderComponent from "@/components/header";
+
+function DoNotSellMyInformation() {
+  return (
+    <>
+      <HeaderComponent />
+      <div>Welcome to Do Not Sell My Information Page</div>
+      <FooterComponent />
+    </>
+  );
+}
+
+export default DoNotSellMyInformation;
