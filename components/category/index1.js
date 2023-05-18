b78095337dc193067d25da1e826694fc06cb69cc
Ankit Kumar Sachan <ankitkumar.sachan@photon.com>
2023-05-18 18:40:54 +0530

    fixed conflicts and navigate product category

--- components/category/index1.js
+++ components/category/index1.js
@@ -1,6 +1,8 @@
 import axios from "axios";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 export default function CategoryComponent({ sectionData }) {
+  const router = useRouter();
   const [response, setResponse] = useState();
   const [loading, setLoading] = useState(true);
 
@@ -29,6 +31,17 @@ export default function CategoryComponent({ sectionData }) {
       });
   }, []);
 
+  function handleClickOnCategory(url) {
+    let f = "?filter=";
+    let splitPath = url !== null ? url?.split(f) : "";
+    router.push({
+      pathname: splitPath[0],
+      query: {
+        filter: splitPath[1],
+      },
+    });
+  }
+
   return (
     <div className="container w-full mx-auto my-6 mb-0 lg:mt-20 lg:mb-12 grid grid-cols-2 gap-4 lg:grid-cols-4 px-4 lg:px-0">
       {loading && <p>Loading...</p>}
@@ -37,23 +50,44 @@ export default function CategoryComponent({ sectionData }) {
         response &&
         response.map((item) => {
           return (
-            <div id="category" key={item?.data?.contentLink?.id} className="mb-6">
-              <div className="mx-auto w-36 lg:w-52 h-36 lg:h-52 cursor-pointer">
+            <div
+              id="category"
+              key={item?.data?.contentLink?.id}
+              className="mb-6 cursor-pointer"
+            >
+              <div
+                className="mx-auto w-36 lg:w-52 h-36 lg:h-52"
+                onClick={() =>
+                  handleClickOnCategory(item?.data?.productCategoryUrl?.value)
+                }
+              >
                 <style jsx>{`
                   .border {
                     border: 2px solid ${item?.data?.backgroundColor?.value};
                   }
                 `}</style>
-                <img src={item?.data?.productCategoryImage?.value?.url} alt="category image" id={item?.data?.productCategoryImage?.value?.url} tabIndex={0}/>
+                <img
+                  src={item?.data?.productCategoryImage?.value?.url}
+                  alt="category image"
+                  id={item?.data?.productCategoryImage?.value?.url}
+                  tabIndex={0}
+                />
               </div>
-              <div className="text-center text-gtl-med text-xl lg:text-2xl mt-6 lg:mt-10 text-mckblue cursor-pointer" tabIndex={0} id={item?.data?.name}>
+              <div
+                className="text-center text-gtl-med text-xl lg:text-2xl mt-6 lg:mt-10 text-mckblue cursor-pointer"
+                tabIndex={0}
+                id={item?.data?.name}
+              >
                 {item?.data?.name}
               </div>
               <div
                 className="text-center text-sofia-reg font-normal w-3/4 mx-auto text-base lg:text-lg text-mcknormalgrey"
                 dangerouslySetInnerHTML={{
                   __html: item?.data?.productCategoryDescription?.value,
-                }} tabIndex={0} id={item?.data?.productCategoryDescription?.value}></div>
+                }}
+                tabIndex={0}
+                id={item?.data?.productCategoryDescription?.value}
+              ></div>
             </div>
           );
         })}
