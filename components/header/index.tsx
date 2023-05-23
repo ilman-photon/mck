62c332f92111df861eb42a9577af682d6945ae9d
bharath <bharath.r1@photon.in>
2023-05-23 20:21:32 +0530

    UI-changes

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -141,7 +141,7 @@ function HeaderComponent() {
         onMouseEnter={handleHeaderMouseEnter}
         onMouseLeave={handleHeaderMouseLeave}
         id="header"
-        className="header flex lg:grid container sticky mx-auto blue-txt bg-mckwhite"
+        className="header flex lg:grid container sticky mx-auto blue-txt border-b bg-mckwhite"
       >
         
         <div className="flex" style={!isMobile ? divHeight : undefined}>
