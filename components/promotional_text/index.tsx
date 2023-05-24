d75740726c2cfa7eac835e01316def2f85ee7c3e
hisman.salick <hisman.salick@photon.com>
2023-05-24 15:08:54 +0530

    FAndT Assect handled

--- components/promotional_text/index.tsx
+++ components/promotional_text/index.tsx
@@ -2,18 +2,18 @@ function ImageVideoOrTextSection({ sectionData, textAlignment }: any) {
   return (
     <>
       <div
-        className={`w-full container lg:pb-6 pb-6 lg:pt-6 ${textAlignment} mx-auto`}
+        className={`w-full container lg:pb-6 pb-6 lg:pt-6 ${textAlignment === 'text-center' && textAlignment} mx-auto`}
         key={sectionData?.contentLink?.id}
       >
         <h3
           // id="promotion-heading"
-          className="text-2xl lg:text-5xl text-gtl-med blue-txt lg:mt-6" tabIndex={0} id={sectionData?.title?.value}
+          className="text-2xl lg:text-5xl text-gtl-med blue-txt lg:mt-6 text-center" tabIndex={0} id={sectionData?.title?.value}
         >
           {sectionData?.title?.value}
         </h3>
         <div
           // id="promotion-text"
-          className="mx-auto w-full lg:w-1/2 px-3 text-lg lg:mt-8 mt-4 text-sofia-reg grey-txt"
+          className={`mx-auto w-full lg:${textAlignment === 'text-center' && 'w-1/2' } px-3 text-lg lg:mt-8 mt-4 text-sofia-reg grey-txt ${textAlignment} `}
           dangerouslySetInnerHTML={{ __html: sectionData?.description?.value }} tabIndex={0} id={sectionData?.description?.value}
         ></div>
       </div>
