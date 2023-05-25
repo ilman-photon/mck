536fc4c133913a5390ca0026c9a12387987303f9
Arunkumar <arunkumar.mahendran@photon.com>
2023-05-25 11:19:18 +0530

    1134 fixes

--- components/health_needs/healthNeedCategory.tsx
+++ components/health_needs/healthNeedCategory.tsx
@@ -50,7 +50,7 @@ const HealthNeedCategory = ({
   return (
     <div className="hidden lg:flex md:flex">
       <div
-        className="health-needs-categories pt-9 px-1.5 mt-12 pb-0 bg-[#f2f4f9]"
+        className="health-needs-categories pt-9 px-1.5 mt-[72px] pb-0 bg-[#f2f4f9]"
         tabIndex={0}
         id="hn_label_002"
       >
