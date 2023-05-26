8f0a3a92e7a6880c2c678085cac99d21f3251d87
Rajaram Sathiyamoorthy (Photon) <rajaram.sathiyamoorthy@photon.in>
2023-05-26 21:51:05 +0530

    ADA fixies

--- components/pdp_location/index.tsx
+++ components/pdp_location/index.tsx
@@ -101,7 +101,7 @@ function PdpLocation(props: any) {
                 //  onChange={(e)=>setTextInput(e.target.value) }
                 placeholder="City, State or Zip code"
                 onKeyDown={(e) => handleKey(e)}
-                className="pl-[46px] flex flex-row rounded-full lg:rounded border border-solid border-mcklggrey items-center gap-2 w-full h-12 bg-blue text-mckgreyborder bg-[#F8F9FB] pl-9"  title="Search" role="combobox" />
+                className="pl-[46px] flex flex-row rounded-full lg:rounded border border-solid border-mcklggrey items-center gap-2 w-full h-12 bg-blue text-mckgreyborder bg-[#F8F9FB] pl-9"  title="Search" />
                 <img src="images\search_btn.png" alt="img" className="absolute left-4 top-4" />
             </div>
             
