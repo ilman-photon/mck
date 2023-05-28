e66ca03323158155f87662b8778af25f63512cc4
rajaram.sathiyamoorthy <rajaram.sathiyamoorthy@photon.com>
2023-05-28 11:45:11 +0530

    added check for bg image

--- components/503/503.tsx
+++ components/503/503.tsx
@@ -28,10 +28,10 @@ function ServiceUnavailableCoumponent() {
         <>
             <div className="err-page-503 container flex lg:flex-row  mx-auto lg:mt-36 lg:pl-18 lg:pb-54 pt-6 pl-4 pr-4 lg:pt-0 lg:pr-0">
                 <div className="lg:w-9/12 w-full lg:pt-18">
-                <h2 className="lg:heading pb-3 text-54 text-gtl-med font-medium text-mckblue" id="err-503-label-001">{serviceErrorData?.data[0]?.title.value}</h2>
-                <div className="lg:text error-msg pb-6 font-medium text-mckblue text-gtl-med" id="err-503-label-002" dangerouslySetInnerHTML={{ __html: serviceErrorData?.data[0]?.description.value}}></div>
+                <h2 className="lg:heading pb-3 text-54 text-gtl-med font-medium text-mckblue" id="err-503-label-001">{serviceErrorData?.data[0]?.title?.value}</h2>
+                <div className="lg:text error-msg pb-6 font-medium text-mckblue text-gtl-med" id="err-503-label-002" dangerouslySetInnerHTML={{ __html: serviceErrorData?.data[0]?.description?.value}}></div>
                 </div>
-                <div className="lg:h-502 overflow-hidden bg-logo-right-503"><img src={serviceErrorData?.data[0]?.backgroundImage?.expandedValue.url} alt="oops" id="err-503-img-005"/></div>
+                <div className="lg:h-502 overflow-hidden bg-logo-right-503"><img src={serviceErrorData?.data[0]?.backgroundImage?.expandedValue?.url} alt="" id="err-503-img-005"/></div>
             </div>  
         </>
     );
