221d8dbbbef6ab189c0ff0641922e7b1e4491b0a
ilman <ilman.teguh@photon.com>
2023-05-30 13:30:41 +0700

    BUG 1447

--- components/recommendational_product/index.tsx
+++ components/recommendational_product/index.tsx
@@ -42,26 +42,420 @@ function RecommendationalProductComponent({ sectionData, indexs }: any) {
     const desiredURL = `/selected_product_category?filter=${encodedValue}/${encodedValue}`;
     router.push(desiredURL);
   };
+  const tempResponse = [
+    {
+      data: {
+        contentLink: {
+          id: 133,
+          workId: 0,
+          guidValue: "1ab653f8-f0d8-47b8-9485-1348150e55c2",
+          providerName: null,
+          url: null,
+          expanded: null,
+        },
+        name: "Foster & Thrive1",
+        language: {
+          link: null,
+          displayName: "English",
+          name: "en",
+        },
+        existingLanguages: [
+          {
+            link: null,
+            displayName: "English",
+            name: "en",
+          },
+        ],
+        masterLanguage: {
+          link: null,
+          displayName: "English",
+          name: "en",
+        },
+        contentType: ["Block", "RecommendedProductItemsBlock"],
+        parentLink: {
+          id: 3,
+          workId: 0,
+          guidValue: "e56f85d0-e833-4e02-976a-2d11fe4d598c",
+          providerName: null,
+          url: "https://mcco02mstrub73kinte.dxcloud.episerver.net/globalassets/",
+          expanded: null,
+        },
+        routeSegment: null,
+        url: null,
+        changed: "2023-04-20T18:02:34Z",
+        created: "2023-04-20T18:01:23Z",
+        startPublish: "2023-04-20T18:02:34Z",
+        stopPublish: null,
+        saved: "2023-05-27T09:29:53Z",
+        status: "Published",
+        category: {
+          value: [],
+          propertyDataType: "PropertyCategory",
+        },
+        internalTitle: {
+          value: "<p>You partner in care.</p>",
+          propertyDataType: "PropertyXhtmlString",
+        },
+        imageTitle: {
+          value: {
+            id: 262,
+            workId: 0,
+            guidValue: "7c0e3992-570f-455c-8ebf-803357bc620f",
+            providerName: null,
+            url: "https://mcco02mstrub73kinte.dxcloud.episerver.net/globalassets/ft_logo_acute.png",
+            expanded: null,
+          },
+          propertyDataType: "PropertyContentReference",
+        },
+        description: {
+          value:
+            "<p>You&rsquo;re in touch with your health, able to pinpoint what you need. With Foster &amp; Thrive, you can easily target and treat your precise needs Test.</p>",
+          propertyDataType: "PropertyXhtmlString",
+        },
+        image: {
+          value: {
+            id: 214,
+            workId: 0,
+            guidValue: "0dee127f-3e91-45d4-9599-973257e134f0",
+            providerName: null,
+            url: "https://mcco02mstrub73kinte.dxcloud.episerver.net/globalassets/allergy_relief-.png",
+            expanded: null,
+          },
+          propertyDataType: "PropertyContentReference",
+        },
+        buttonText: {
+          value: "SEE PRODUCTS",
+          propertyDataType: "PropertyLongString",
+        },
+        buttonUrl: {
+          value: "/selected_product_category?filter=Acute Care",
+          propertyDataType: "PropertyUrl",
+        },
+        buttonColor: {
+          value: "#001A71",
+          propertyDataType: "PropertyLongString",
+        },
+        backgroundColor: {
+          value: "#CCD1E3",
+          propertyDataType: "PropertyLongString",
+        },
+        backgroundImage: {
+          value: null,
+          propertyDataType: "PropertyContentReference",
+        },
+      },
+      status: 200,
+      statusText: "OK",
+      headers: {
+        "cache-control": "public, s-maxage=5, stale-while-revalidate=2",
+        "content-type": "application/json; charset=utf-8",
+      },
+      config: {
+        transitional: {
+          silentJSONParsing: true,
+          forcedJSONParsing: true,
+          clarifyTimeoutError: false,
+        },
+        adapter: ["xhr", "http"],
+        transformRequest: [null],
+        transformResponse: [null],
+        timeout: 0,
+        xsrfCookieName: "XSRF-TOKEN",
+        xsrfHeaderName: "X-XSRF-TOKEN",
+        maxContentLength: -1,
+        maxBodyLength: -1,
+        env: {},
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Accept-Language": "en",
+        },
+        method: "get",
+        url: "https://mcco02mstrub73kinte.dxcloud.episerver.net/api/episerver/v3.0/content/133",
+      },
+      request: {},
+    },
+    {
+      data: {
+        contentLink: {
+          id: 134,
+          workId: 0,
+          guidValue: "648f2bac-4508-49c0-80f4-2863e30953f5",
+          providerName: null,
+          url: null,
+          expanded: null,
+        },
+        name: "Foster & Thrive2",
+        language: {
+          link: null,
+          displayName: "English",
+          name: "en",
+        },
+        existingLanguages: [
+          {
+            link: null,
+            displayName: "English",
+            name: "en",
+          },
+        ],
+        masterLanguage: {
+          link: null,
+          displayName: "English",
+          name: "en",
+        },
+        contentType: ["Block", "RecommendedProductItemsBlock"],
+        parentLink: {
+          id: 3,
+          workId: 0,
+          guidValue: "e56f85d0-e833-4e02-976a-2d11fe4d598c",
+          providerName: null,
+          url: "https://mcco02mstrub73kinte.dxcloud.episerver.net/globalassets/",
+          expanded: null,
+        },
+        routeSegment: null,
+        url: null,
+        changed: "2023-04-20T18:04:41Z",
+        created: "2023-04-20T18:03:32Z",
+        startPublish: "2023-04-20T18:04:41Z",
+        stopPublish: null,
+        saved: "2023-05-20T13:07:26Z",
+        status: "Published",
+        category: {
+          value: [],
+          propertyDataType: "PropertyCategory",
+        },
+        internalTitle: {
+          value: "<p>Prepared for what&rsquo;s next.</p>",
+          propertyDataType: "PropertyXhtmlString",
+        },
+        imageTitle: {
+          value: {
+            id: 262,
+            workId: 0,
+            guidValue: "7c0e3992-570f-455c-8ebf-803357bc620f",
+            providerName: null,
+            url: "https://mcco02mstrub73kinte.dxcloud.episerver.net/globalassets/ft_logo_acute.png",
+            expanded: null,
+          },
+          propertyDataType: "PropertyContentReference",
+        },
+        description: {
+          value:
+            "<p>Everybody deserves optimal health, and nobody is immune to everything. With Foster &amp; Thrive, you can be prepared for almost anything.</p>",
+          propertyDataType: "PropertyXhtmlString",
+        },
+        image: {
+          value: {
+            id: 200,
+            workId: 0,
+            guidValue: "86b7383a-4b4a-427e-b87c-d1ce8aa6404c",
+            providerName: null,
+            url: "https://mcco02mstrub73kinte.dxcloud.episerver.net/globalassets/vitamin_d3.png",
+            expanded: null,
+          },
+          propertyDataType: "PropertyContentReference",
+        },
+        buttonText: {
+          value: "SEE PRODUCTS",
+          propertyDataType: "PropertyLongString",
+        },
+        buttonUrl: {
+          value: "/selected_product_category/?filter=Preventative Care",
+          propertyDataType: "PropertyUrl",
+        },
+        buttonColor: {
+          value: "#001A71",
+          propertyDataType: "PropertyLongString",
+        },
+        backgroundColor: {
+          value: "#FFEABC",
+          propertyDataType: "PropertyLongString",
+        },
+        backgroundImage: {
+          value: null,
+          propertyDataType: "PropertyContentReference",
+        },
+      },
+      status: 200,
+      statusText: "OK",
+      headers: {
+        "cache-control": "public, s-maxage=5, stale-while-revalidate=2",
+        "content-type": "application/json; charset=utf-8",
+      },
+      config: {
+        transitional: {
+          silentJSONParsing: true,
+          forcedJSONParsing: true,
+          clarifyTimeoutError: false,
+        },
+        adapter: ["xhr", "http"],
+        transformRequest: [null],
+        transformResponse: [null],
+        timeout: 0,
+        xsrfCookieName: "XSRF-TOKEN",
+        xsrfHeaderName: "X-XSRF-TOKEN",
+        maxContentLength: -1,
+        maxBodyLength: -1,
+        env: {},
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Accept-Language": "en",
+        },
+        method: "get",
+        url: "https://mcco02mstrub73kinte.dxcloud.episerver.net/api/episerver/v3.0/content/134",
+      },
+      request: {},
+    },
+    {
+      data: {
+        contentLink: {
+          id: 134,
+          workId: 0,
+          guidValue: "648f2bac-4508-49c0-80f4-2863e30953f5",
+          providerName: null,
+          url: null,
+          expanded: null,
+        },
+        name: "Foster & Thrive2",
+        language: {
+          link: null,
+          displayName: "English",
+          name: "en",
+        },
+        existingLanguages: [
+          {
+            link: null,
+            displayName: "English",
+            name: "en",
+          },
+        ],
+        masterLanguage: {
+          link: null,
+          displayName: "English",
+          name: "en",
+        },
+        contentType: ["Block", "RecommendedProductItemsBlock"],
+        parentLink: {
+          id: 3,
+          workId: 0,
+          guidValue: "e56f85d0-e833-4e02-976a-2d11fe4d598c",
+          providerName: null,
+          url: "https://mcco02mstrub73kinte.dxcloud.episerver.net/globalassets/",
+          expanded: null,
+        },
+        routeSegment: null,
+        url: null,
+        changed: "2023-04-20T18:04:41Z",
+        created: "2023-04-20T18:03:32Z",
+        startPublish: "2023-04-20T18:04:41Z",
+        stopPublish: null,
+        saved: "2023-05-20T13:07:26Z",
+        status: "Published",
+        category: {
+          value: [],
+          propertyDataType: "PropertyCategory",
+        },
+        internalTitle: {
+          value: "<p>Prepared for what&rsquo;s next.</p>",
+          propertyDataType: "PropertyXhtmlString",
+        },
+        imageTitle: {
+          value: {
+            id: 262,
+            workId: 0,
+            guidValue: "7c0e3992-570f-455c-8ebf-803357bc620f",
+            providerName: null,
+            url: "https://mcco02mstrub73kinte.dxcloud.episerver.net/globalassets/ft_logo_acute.png",
+            expanded: null,
+          },
+          propertyDataType: "PropertyContentReference",
+        },
+        description: {
+          value:
+            "<p>Everybody deserves optimal health, and nobody is immune to everything. With Foster &amp; Thrive, you can be prepared for almost anything.</p>",
+          propertyDataType: "PropertyXhtmlString",
+        },
+        image: {
+          value: {
+            id: 200,
+            workId: 0,
+            guidValue: "86b7383a-4b4a-427e-b87c-d1ce8aa6404c",
+            providerName: null,
+            url: "https://mcco02mstrub73kinte.dxcloud.episerver.net/globalassets/vitamin_d3.png",
+            expanded: null,
+          },
+          propertyDataType: "PropertyContentReference",
+        },
+        buttonText: {
+          value: "SEE PRODUCTS",
+          propertyDataType: "PropertyLongString",
+        },
+        buttonUrl: {
+          value: "/selected_product_category/?filter=Preventative Care",
+          propertyDataType: "PropertyUrl",
+        },
+        buttonColor: {
+          value: "#001A71",
+          propertyDataType: "PropertyLongString",
+        },
+        backgroundColor: {
+          value: "#FFEABC",
+          propertyDataType: "PropertyLongString",
+        },
+        backgroundImage: {
+          value: null,
+          propertyDataType: "PropertyContentReference",
+        },
+      },
+      status: 200,
+      statusText: "OK",
+      headers: {
+        "cache-control": "public, s-maxage=5, stale-while-revalidate=2",
+        "content-type": "application/json; charset=utf-8",
+      },
+      config: {
+        transitional: {
+          silentJSONParsing: true,
+          forcedJSONParsing: true,
+          clarifyTimeoutError: false,
+        },
+        adapter: ["xhr", "http"],
+        transformRequest: [null],
+        transformResponse: [null],
+        timeout: 0,
+        xsrfCookieName: "XSRF-TOKEN",
+        xsrfHeaderName: "X-XSRF-TOKEN",
+        maxContentLength: -1,
+        maxBodyLength: -1,
+        env: {},
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Accept-Language": "en",
+        },
+        method: "get",
+        url: "https://mcco02mstrub73kinte.dxcloud.episerver.net/api/episerver/v3.0/content/134",
+      },
+      request: {},
+    },
+  ];
+
   return (
     <div id="promotion-container" className="container mx-auto lg:px-14">
       <div className="pramotion-product-container">
-        <div className="md:grid lg:grid xl:grid md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 [&>*:nth-last-child(1):nth-child(odd)]:col-span-2">
+        <div className="grid grid-cols-2 grid-rows-2 ">
           {!loading &&
-            response &&
-            response.map((ele: any, index: number) => {
-              let colSpan = "col-span-1";
+            tempResponse &&
+            tempResponse.map((ele: any, index: number) => {
+              let colSpan = "col-span-1 scale-80";
               let rowSpan = "";
-
-              if (response.length === 1) {
+              if (tempResponse.length === 1) {
                 colSpan = "col-span-2";
-              } else if (response.length % 2 === 1 && index === 0) {
+              } else if (tempResponse.length % 2 === 1 && index === 0) {
                 colSpan = "col-span-1";
                 rowSpan = "row-span-2";
               }
-
               return (
                 <div
-                  className={`bg-color lg:p-9 xl:p-9 p-4 sm:m-3 mb-4 ${colSpan} ${rowSpan} `}
+                  className={`bg-color  lg:p-9 xl:p-9 p-4 sm:m-3 mb-4 bg-red ${colSpan} ${rowSpan} `}
                   key={ele?.data?.contentLink?.id}
                 >
                   <style jsx>{`
@@ -71,8 +465,9 @@ function RecommendationalProductComponent({ sectionData, indexs }: any) {
                   `}</style>
 
                   <div
-                    className={`grid h-full ${colSpan === "col-span-2" ? "place-items-center" : ""
-                      }`}
+                    className={`grid h-full ${
+                      colSpan === "col-span-2" ? "place-items-center" : ""
+                    }`}
                   >
                     <div className="w-full lg:w-44 lg:mb-8 mb-6 lg:min-h-57">
                       <img
@@ -83,7 +478,6 @@ function RecommendationalProductComponent({ sectionData, indexs }: any) {
                         id={"home-product-image" + indexs + index + 1}
                       />
                     </div>
-
                     <div className="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 lg:gap-4 xl:gap-4 lg:pr-3 my-auto text-justify">
                       <div className="pb-4 lg:pb-0 col-span-1">
                         <div className="mx-auto my-auto lg:h-60 object-contain">
@@ -100,10 +494,11 @@ function RecommendationalProductComponent({ sectionData, indexs }: any) {
 
                       <div
                         id={"p-text" + indexs + index + 1}
-                        className={`text-justify pr-0 lg:pr-9 col-span-2 ${colSpan === "col-span-2"
-                          ? "flex justify-center items-center"
-                          : ""
-                          }`}
+                        className={`text-justify pr-0 lg:pr-9 col-span-2 ${
+                          colSpan === "col-span-2"
+                            ? "flex justify-center items-center"
+                            : ""
+                        }`}
                       >
                         <div
                           className="text-lg text-sofia-reg text-mcknormalgrey font-normal text-left mb-4 text-sixcontent-ellipsis"
--- pages/product_detail/index.tsx
+++ pages/product_detail/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import FooterComponent from "@/components/footer";
 import HeaderComponent from "@/components/header";
 import PdpCarousel from "@/components/pdp_carousel";
@@ -7,74 +7,108 @@ import HeaderNoStickyComponent from "@/components/header_nosticky";
 import PdpLocation from "@/components/pdp_location";
 import GoogleTagManager from "@/components/google_tag_manager";
 import Head from "next/head";
+import gifImage from "../../public/images/FT-2593651-0423 Foster & Thrive Animated gif_circle.gif";
+import Image from "next/image";
 interface MyComponentProps {
-    Response: {
-      data: {
-        title: {
-          value: string;
-        };
-      }[];
-    };
-  }
-  
+  Response: {
+    data: {
+      title: {
+        value: string;
+      };
+    }[];
+  };
+}
+
 function ProductDetails({ Response }: MyComponentProps): React.ReactElement {
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [apiRespond, setApiRespond] = useState<any>();
+  const [apiRespond, setApiRespond] = useState<any>();
 
-    useEffect(() => {
-      document.documentElement.lang = 'en';
-    }, []);
-  
-    useEffect(() => {
-        if (Response && Response.data && Response.data.length > 0 && Response.data[0].title && Response.data[0].title.value) {
-          fetch('https://api.example.com/data')
-            .then((response: Response) => {
-              if (!response.ok) {
-                throw new Error('Request failed');
-              }
-              return response.json();
-            })
-            .then((data: any) => {
-              if (data && data.length > 0 && data[0].title && data[0].title.value) {
-                document.title = data[0].title.value;
-              } else {
-                document.title = "Product Details Page";
-              }
-            })
-            .catch((error: Error) => {
-              console.log(error.message);
-              document.title = "Product Details Page";
-            });
-        } else {
+  useEffect(() => {
+    document.documentElement.lang = "en";
+  }, []);
+  useEffect(() => {
+    // Simulating a delay to show the loading state
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
+  useEffect(() => {
+    if (
+      Response &&
+      Response.data &&
+      Response.data.length > 0 &&
+      Response.data[0].title &&
+      Response.data[0].title.value
+    ) {
+      fetch("https://api.example.com/data")
+        .then((response: Response) => {
+          if (!response.ok) {
+            throw new Error("Request failed");
+          }
+          return response.json();
+        })
+        .then((data: any) => {
+          if (data && data.length > 0 && data[0].title && data[0].title.value) {
+            document.title = data[0].title.value;
+          } else {
+            document.title = "Product Details Page";
+          }
+        })
+        .catch((error: Error) => {
+          console.log(error.message);
           document.title = "Product Details Page";
-        }
-      }, [JSON.stringify(Response)]);
+        });
+    } else {
+      document.title = "Product Details Page";
+    }
+  }, [JSON.stringify(Response)]);
 
-    return (
-        <>
-        <Head>
-            <title>Your Page Title</title>
-          </Head>
-            <GoogleTagManager/>
-            {/* <HeaderNoStickyComponent /> */}
-            <HeaderComponent />
-            <div className="product-detail-page grid lg:grid-flow-col container lg:px-72 lg:mx-4 lg:mx-auto mt-18">
-                <div className="left-column overflow-hidden lg:overflow-visible">
-                <div className="pdp-slider">
-                <PdpCarousel/>
-                </div>
-                <div className="hidden lg:block">
-                <PdpLocation index={1}/>    
-                </div>
-                </div>
-                <PdpDetail/>
-                <div className="lg:hidden">
-                <PdpLocation index={2}/>    
-                </div>
+  return (
+    <>
+      <Head>
+        <title>Your Page Title</title>
+      </Head>
+      <GoogleTagManager />
+      <HeaderComponent />
+      {isLoading ? (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="fixed inset-0 bg-black opacity-75"></div>
+          <div
+            className="relative"
+            style={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+          >
+            <Image
+              src={gifImage}
+              alt="loading-image"
+              width={400}
+              height={400}
+              loading="eager"
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="product-detail-page grid lg:grid-flow-col container lg:px-72 lg:mx-4 lg:mx-auto mt-18">
+          <div className="left-column overflow-hidden lg:overflow-visible">
+            <div className="pdp-slider">
+              <PdpCarousel />
             </div>
-            <FooterComponent />
-        </>
-    );
+            <div className="hidden lg:block">
+              <PdpLocation index={1} />
+            </div>
+          </div>
+          <PdpDetail />
+          <div className="lg:hidden">
+            <PdpLocation index={2} />
+          </div>
+        </div>
+      )}
+
+      <FooterComponent />
+    </>
+  );
 }
 
 export default ProductDetails;
