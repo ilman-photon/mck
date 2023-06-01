96ade8679fcd17d7cea7821474ae91a12066b141
vasudevan.v <vasudevan.v@photon.com>
2023-06-01 20:43:59 +0530

    blog product detail

--- components/blog_details/RelatedProducts.tsx
+++ components/blog_details/RelatedProducts.tsx
@@ -15,13 +15,13 @@ const RelatedProducts: React.FC<RelatedProductsProps> = ({ BlogListingContent, t
                 <div
                     onClick={() => OnRelatedProductClick(item)}
                     key={index}
-                    className="shade-blue-border rounded-lg overflow-hidden lg:mb-6 mb-4 pt-4">
-                    <figure className='w-full text-center'>
+                    className="shade-blue-border rounded-lg overflow-hidden lg:mb-6 mb-4 pt-4 lg:max-h-[377px] ">
+                    <figure className='w-full  text-center'>
                         <ImageComponent
                             src={item.image.expandedValue?.url}
                             id='blog-img-006'
                             alt='Allergy Relief Product'
-                            className="px-5 lg:px-0 m-auto lg:w-32"
+                            className="px-5 lg:px-0 m-auto lg:w-32 lg:max-h-60"
                         />
                     </figure>
                     <figcaption>
@@ -37,12 +37,12 @@ const RelatedProducts: React.FC<RelatedProductsProps> = ({ BlogListingContent, t
                             </div>
 
                             <a href=""
-                                className="articleTitle text-xl text-sofia-reg font-extrabold text-mckblue mt-3 mb-1 no-underline"
+                                className="articleTitle text-xl text-heading-ellipsis text-sofia-reg font-extrabold text-mckblue mt-3 mb-1 no-underline"
                                 aria-labelledby="CoverMyMeds Leaders Analyze 4 Key Trends from Medication Access Report"
                                 id={`blog-label-010-` + index}>
                                 {item.name}
                             </a>
-                            <p className="text-sofia-reg font-noraml text-mcknormalgrey text-base" id='blog-label-010-03' dangerouslySetInnerHTML={{ __html: item.subTitle.value }}></p>
+                            <p className="text-sofia-reg text-heading-ellipsis font-noraml text-mcknormalgrey text-base" id='blog-label-010-03' dangerouslySetInnerHTML={{ __html: item.subTitle.value }}></p>
                         </div>
                     </figcaption>
                 </div>))}
