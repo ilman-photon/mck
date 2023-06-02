3a91d473d24618393ece91b0e22f035d4b851292
Your Name <you@example.com>
2023-06-02 13:40:13 +0530

    header menu hover issue

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -14,7 +14,7 @@ function NavBar({ menuData, isMobileMenuActive }: Props) {
         id="nav-bar"
         className={`md:flex container lg:flex ml-auto mobile-customenav ${isMobileMenuActive ? "mobile-overlay-wrapper" :""}`}
       >
-        <div className={`lg:mx-auto lg:flex xl:mx-auto xl:flex isMobileUi mobile-navwrapper ${isMobileMenuActive ? "active bg-mcklightyellow text-mckblue" : ""}`}>
+        <div className={`lg:mx-auto lg:flex xl:mx-auto xl:flex isMobileUi mobile-navwrapper lg:bg-transparent ${isMobileMenuActive ? "active bg-mcklightyellow text-mckblue" : ""}`}>
           {menuData?.map((item: any) => {
             return (
               <div
