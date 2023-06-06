c5ff355789dc32d7535d60bd3cd830c73a5d547b
vasudevan.v <vasudevan.v@photon.com>
2023-06-06 16:02:54 +0530

    Blog other search results

--- components/blog/BlogListContainer.tsx
+++ components/blog/BlogListContainer.tsx
@@ -43,12 +43,12 @@ const BlogListContainer = () => {
                                                 query: { id: item.routeSegment },
                                             }}
                                         >
-                                            <div className="h-314 flex">
-                                                <ImageComponent src={item.image.value.url} className='max-h-314 w-full' alt={item.image.value.url} id={item.image.value.id} />
+                                            <div className="lg:h-314 h-240 flex">
+                                                <ImageComponent src={item.image.value.url} className='lg:max-h-314 max-h-240 w-full' alt={item.image.value.url} id={item.image.value.id} />
                                             </div>
                                         </LinkComponent>
                                     </figure>
-                                    <figcaption className="lg:h-260 h-225">
+                                    <figcaption className="lg:h-260 h-210">
                                         <div className='content lg:p-6 p-4 lg:pt-4'>
                                             <LinkComponent
                                                 href={{
@@ -56,7 +56,7 @@ const BlogListContainer = () => {
                                                     query: { id: item.routeSegment },
                                                 }}
                                             >
-                                                <p className='articleTitle lg:text-32 text-3xl text-gtl-med text-mckblue mb-3 no-underline lg:leading-9 lg:h-32 h-28 text-p-ellipsis' aria-labelledby={item.title.value}>{item.title.value}</p>
+                                                <p className='articleTitle lg:text-32 text-xl text-gtl-med text-mckblue mb-3 no-underline lg:leading-9 lg:h-32  text-p-ellipsis' aria-labelledby={item.title.value}>{item.title.value}</p>
                                             </LinkComponent>
                                             <div className='pb-3 pt-3'>
                                                 <span className={`text-mcknormalgrey text-sofia-reg font-normal lg:text-base text-sm pr-2 border-solid ${item.readMinute.value ? 'shade-grey-right-border' : ''}`}>{GetTime(item.startPublish)}</span>
