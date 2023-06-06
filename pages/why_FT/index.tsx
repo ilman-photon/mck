0530727be39c21c42a64a6978fb5e0734f892720
ilman <ilman.teguh@photon.com>
2023-06-06 16:11:45 +0700

    BUG 1623

--- pages/why_FT/index.tsx
+++ pages/why_FT/index.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import WhyFTComponent from "@/components/why_FT/WhyFTComponent";
 import LandingLayout from "@/components/Layout/LandingLayout";
+import FooterComponent from "@/components/footer";
+import HealthCareProfessionalComponent from "@/components/health_care_professional";
+import GoogleTagManager from "@/components/google_tag_manager";
+import HeaderComponent from "@/components/header";
 
 function FTComponent() {
-    return (
-        <LandingLayout>
-            <WhyFTComponent />
-        </LandingLayout>
-    )
-
+  const [isCarusonAvible, setisCarusonAvible] = useState(false);
+  return (
+    <>
+      <GoogleTagManager />
+      <HeaderComponent isCarusolAvaible={isCarusonAvible} />
+      <WhyFTComponent isCarusolAvaibleProps={setisCarusonAvible} />
+      <FooterComponent />
+    </>
+  );
 }
 export default FTComponent;
