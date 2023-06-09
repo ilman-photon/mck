40f2df9e38ac7ce61969b9e28f7a282a4e9ed335
Bharath Raju <bharath.raju@photon.com>
2023-06-09 20:25:38 +0530

    Added sanitize

--- components/404/404.tsx
+++ components/404/404.tsx
@@ -1,6 +1,7 @@
 import axiosInstance from "@/utils/axiosInstance";
 import axios, { AxiosError } from "axios";
 import { useState, useEffect } from "react";
+import DOMPurify from 'isomorphic-dompurify';
 
 function FourOhFourCoumponent() {
     const [notFoundData, SetNotFoundData] = useState<any>();
@@ -22,11 +23,11 @@ console.log(notFoundData, "error")
         <>
             <div className="four-oh-four err-page-404 container flex flex-row mx-auto">
                 <div className="lg:width-416 w-full lg:pt-72">
-                <div className="heading pb-3 text-[54px] leading-[61px] text-gtl-med font-medium text-mckblue" id="err-404-label-001"  aria-label={notFoundData?.data[0]?.title?.value}>{notFoundData?.data[0]?.title?.value}</div>
-                <div className="error-msg pb-6 font-medium lg:text-4xl text-xl text-2xl text-mckblue text-gtl-med" id="err-404-label-002" dangerouslySetInnerHTML={{ __html: notFoundData?.data[0]?.description?.value}}  aria-label={notFoundData?.data[0]?.description?.value}></div>
+                <div className="heading pb-3 text-[54px] leading-[61px] text-gtl-med font-medium text-mckblue" id="err-404-label-001"  aria-label={DOMPurify.sanitize(notFoundData?.data[0]?.title?.value)}>{DOMPurify.sanitize(notFoundData?.data[0]?.title?.value)}</div>
+                <div className="error-msg pb-6 font-medium lg:text-4xl text-xl text-2xl text-mckblue text-gtl-med" id="err-404-label-002" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(notFoundData?.data[0]?.description?.value)}}  aria-label={DOMPurify.sanitize(notFoundData?.data[0]?.description?.value)}></div>
                 <div className="table-container rounded-lg lg:w-[416px]">
-                    <div className="text-base table-heading lg:text-lg text-mckblue font-extrabold text-sofia-bold" id="err-404-label-003"  aria-label={notFoundData?.data[0]?.contentArea?.expandedValue[0].title?.value}>{notFoundData?.data[0]?.contentArea?.expandedValue[0]?.title?.value}</div>
-                    <div className="normal-text p-4 lg:p-6 text-sofia-reg font-normal lg:text-lg text-base" dangerouslySetInnerHTML={{ __html: notFoundData?.data[0]?.contentArea?.expandedValue[0]?.description?.value}}  aria-label={notFoundData?.data[0]?.contentArea?.expandedValue[0]?.description?.value}></div>
+                    <div className="text-base table-heading lg:text-lg text-mckblue font-extrabold text-sofia-bold" id="err-404-label-003"  aria-label={DOMPurify.sanitize(notFoundData?.data[0]?.contentArea?.expandedValue[0].title?.value)}>{DOMPurify.sanitize(notFoundData?.data[0]?.contentArea?.expandedValue[0]?.title?.value)}</div>
+                    <div className="normal-text p-4 lg:p-6 text-sofia-reg font-normal lg:text-lg text-base" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(notFoundData?.data[0]?.contentArea?.expandedValue[0]?.description?.value)}}  aria-label={DOMPurify.sanitize(notFoundData?.data[0]?.contentArea?.expandedValue[0]?.description?.value)}></div>
                 </div>
                 </div>
                 <div className="bg-logo-right overflow-hidden lg:relative absolute">
