55d9b801895d30d346a3c406a5c079555cf83e91
hisman.salick <hisman.salick@photon.com>
2023-06-12 13:55:46 +0530

    Blog optional chaining

--- components/blog/BlogListContainer.tsx
+++ components/blog/BlogListContainer.tsx
@@ -57,11 +57,11 @@ const BlogListContainer = () => {
                                                     query: { id: item?.routeSegment },
                                                 }}
                                             >
-                                                <p className='articleTitle lg:text-32 text-xl text-gtl-med text-mckblue mb-3 no-underline lg:leading-9 lg:h-28  text-p-ellipsis' aria-labelledby={item.title.value}>{item.title.value}</p>
+                                                <p className='articleTitle lg:text-32 text-xl text-gtl-med text-mckblue mb-3 no-underline lg:leading-9 lg:h-28  text-p-ellipsis' aria-labelledby={item.title.value}>{item?.title?.value}</p>
                                             </LinkComponent>
                                             <div className='pb-3 pt-3'>
-                                                <span className={`text-mcknormalgrey text-sofia-reg font-normal lg:text-base text-sm pr-2 border-solid ${item?.readMinute?.value ? 'shade-grey-right-border' : ''}`}>{GetTime(item.startPublish)}</span>
-                                                <span className={`text-mcknormalgrey text-sofia-reg font-normal lg:text-base text-sm px-2 border-solid ${false ? 'shade-grey-right-border' : ''}`}>{item.readMinute.value}</span>
+                                                <span className={`text-mcknormalgrey text-sofia-reg font-normal lg:text-base text-sm pr-2 border-solid ${item?.readMinute?.value ? 'shade-grey-right-border' : ''}`}>{GetTime(item?.startPublish)}</span>
+                                                <span className={`text-mcknormalgrey text-sofia-reg font-normal lg:text-base text-sm px-2 border-solid ${false ? 'shade-grey-right-border' : ''}`}>{item?.readMinute?.value}</span>
                                                 {/* <span className='text-mckblue text-sofia-reg font-normal lg:text-base text-sm pl-2'>1.3K views</span> */}
                                             </div>
                                             <div className='flex flex-wrap h-25 overflow-hidden'>
