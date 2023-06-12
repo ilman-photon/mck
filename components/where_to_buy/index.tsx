026ff4aa5d943dc567f7ccf7da73f99dbf170820
vasudevan.v <vasudevan.v@photon.com>
2023-06-12 19:23:27 +0530

    scroll issue for mobile

--- components/where_to_buy/index.tsx
+++ components/where_to_buy/index.tsx
@@ -186,7 +186,7 @@ function WhereComponent() {
             >
               Disclaimer: Products are subject to availability
             </div>
-            <div className="lg:w-[640px] lg:desktop:w-[500px] pb-6 pl-6 lg:pr-4 pr-4 overflow-y-scroll lg:h-689 mr-6 location-box">
+            <div className="lg:w-[640px] lg:desktop:w-[500px] mobile:h-[551px] pb-6 pl-6 lg:pr-4 pr-4 overflow-y-scroll lg:h-689 mr-6 location-box">
               {responseValue?.map((value: any, index: Number) => {
                 return (
                   <div
