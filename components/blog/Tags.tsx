6acf41141dcf5447d4b08556c4618554e9ee0908
hisman.salick <hisman.salick@photon.com>
2023-06-13 18:29:41 +0530

    Blog Tag Changes

--- components/blog/Tags.tsx
+++ components/blog/Tags.tsx
@@ -1,14 +1,17 @@
 import { memo } from "react";
 import DOMPurify from 'isomorphic-dompurify';
-const TagsComponent = ({BlogInfo}: any) => {
+import { handlecategoryColorCode } from "../global/CommonUtil";
+import { useHeaderStore } from "../navbar/Store/useNavBarStore";
+const TagsComponent = ({BlogTag}: any) => {
+  const catMapping = useHeaderStore((state) => state.categoryMapping);
   return (
     <div className="flex flex-wrap">
-      {BlogInfo?.tag?.value.map((item: any, index: any) => (
+      {BlogTag?.value.map((item: any, index: any) => (
         <div
           key={index}
           style={{
-            backgroundColor: BlogInfo?.tagBackgroundColorCode?.value,
-          }}
+            backgroundColor: handlecategoryColorCode(item.id, catMapping),
+        }}
           className="categoryTag mb-1.5 text-mckblue text-sofia-reg font-extrabold text-xs rounded-lg w-fit py-0.5 px-2 ml-1 border-solid shade-blue-border"
           id="blog-label-006"
         >
