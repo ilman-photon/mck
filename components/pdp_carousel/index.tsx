65b71c64bd0376b39b0c0282e5536b7dbc952a7f
ilman <ilman.teguh@photon.com>
2023-06-13 13:39:06 +0700

    1724

--- components/pdp_carousel/index.tsx
+++ components/pdp_carousel/index.tsx
@@ -12,7 +12,7 @@ function PdpCarousel(prodViewData: any) {
   const [selectedItemIndex, setSelectedItemIndex] = useState(0);
   const [lastIndex, setLastIndex] = useState(0);
   const [arrowClick, setArrowClick] = useState(0);
-
+  const [dataImageId, setDataImageId] = useState(0);
   const [prodResponse, setProdResponse] = useState<any>();
 
   function fetchPDPCarouselDetails() {
@@ -37,7 +37,7 @@ function PdpCarousel(prodViewData: any) {
 
   useEffect(() => {
     fetchPDPCarouselDetails()
-      .then((res:any) => {
+      .then((res: any) => {
         setProdResponse(res.data[0]);
       })
       .catch((e: Error | AxiosError) => console.log(e));
@@ -47,72 +47,75 @@ function PdpCarousel(prodViewData: any) {
     if (lastIndex < prodResponse?.productImages?.value?.length) {
       setArrowClick(() => arrowClick + 1);
       setLastIndex(() => lastIndex + 1);
-    } else {
-      prodResponse?.productImages?.value?.slice(arrowClick, lastIndex).length <=
-        arrowClick && setArrowClick(() => arrowClick + 1);
     }
+    prodResponse?.productImages?.value?.length > selectedItemIndex &&
+      setSelectedItemIndex((prevState) => prevState + 1);
   };
 
   const handleUpArrowClick = () => {
     if (arrowClick > 0) {
       setArrowClick(() => arrowClick - 1);
       setLastIndex(() => lastIndex - 1);
-    } else {
-      selectedItemIndex - 1 >= 0 && setArrowClick(() => arrowClick - 1);
     }
+    selectedItemIndex > 0 && setSelectedItemIndex((prevState) => prevState - 1);
   };
 
-  const handleImageClick = (i: any) => {
-    setSelectedItemIndex(i);
+  const handleImageClick = (i: any, dataImageId?: any) => {
+    const index = prodResponse?.productImages?.value.findIndex(
+      (imgdata: any) => imgdata.id === dataImageId
+    );
+    setSelectedItemIndex(index);
+    setDataImageId(dataImageId);
   };
 
   const handleMouseOver = (id: number) => {
-    setSelectedItemIndex(id);
+    // setSelectedItemIndex(id);
   };
 
-  const isProductImageLessThanSix = prodResponse?.productImages?.expandedValue?.length < 6
-
+  const isProductImageLessThanSix =
+    prodResponse?.productImages?.expandedValue?.length < 6;
+  console.log(prodResponse?.productImages?.value);
   return (
     <div className="flex lg:mx-auto lg:h-[636px] mx-4 lg:mx-0" id="pdp-01">
       <div className="flex flex-col-reverse lg:flex-row pdp-carousel w-full">
         <div className="pdp_images flex lg:grid relative">
-         {isProductImageLessThanSix ? null : (
-          <div className="hidden lg:block cursor-pointer absolute left-[36px] top-[-13px]">
-            <img
-              onClick={handleUpArrowClick}
-              src="images\carousel_down.png"
-              alt="img"
-              className={`m-auto ${
-                arrowClick === 0 ? "opacity-25" : ""
-              } rotate-180`}
-              id="pdp-img-001"
-            />
-          </div>
-         )}   
-         {isProductImageLessThanSix ? null : (
+          {isProductImageLessThanSix ? null : (
+            <div className="hidden lg:block cursor-pointer absolute left-[36px] top-[-13px]">
+              <img
+                onClick={handleUpArrowClick}
+                src="images\carousel_down.png"
+                alt="img"
+                className={`m-auto ${
+                  selectedItemIndex <= 0 ? "opacity-25" : ""
+                } rotate-180`}
+                id="pdp-img-001"
+              />
+            </div>
+          )}
+          {isProductImageLessThanSix ? null : (
             <div
-            onClick={handleUpArrowClick}
-            className={`carousel-prev -left-6 cursor-pointer lg:hidden flex items-center absolute left-[-10px] top-[27px] ${
-              arrowClick === 0 ? "opacity-25" : ""
-            }`}
-          >
-            <svg
-              className="focus:outline-none"
-              width="24"
-              height="49"
-              viewBox="0 0 48 49"
-              id="hcp-btn-005"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+              onClick={handleUpArrowClick}
+              className={`carousel-prev -left-6 cursor-pointer lg:hidden flex items-center absolute left-[-10px] top-[27px] ${
+                selectedItemIndex <= 0 ? "opacity-25" : ""
+              }`}
             >
-              <circle cx="24" cy="24.8306" r="24" fill="#001A71"></circle>
-              <path
-                d="M28.9401 18.7106L27.0601 16.8306L19.0601 24.8306L27.0601 32.8306L28.9401 30.9506L22.8334 24.8306L28.9401 18.7106Z"
-                fill="#ffffff"
-              ></path>
-            </svg>
-          </div>
-         )} 
+              <svg
+                className="focus:outline-none"
+                width="24"
+                height="49"
+                viewBox="0 0 48 49"
+                id="hcp-btn-005"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="24" cy="24.8306" r="24" fill="#001A71"></circle>
+                <path
+                  d="M28.9401 18.7106L27.0601 16.8306L19.0601 24.8306L27.0601 32.8306L28.9401 30.9506L22.8334 24.8306L28.9401 18.7106Z"
+                  fill="#ffffff"
+                ></path>
+              </svg>
+            </div>
+          )}
           {/* <button onClick={handleUpArrowClick}>Up arrow</button> */}
           <ul className="3GnUWp flex lg:flex-col">
             {prodResponse?.productImages?.value
@@ -131,7 +134,7 @@ function PdpCarousel(prodViewData: any) {
                     // key={imgdata?.id}
                     key={`pdpcarousalkey${index}`}
                     onClick={() => {
-                      handleImageClick(index);
+                      handleImageClick(index, imgdata?.id);
                     }}
                     onMouseOver={(event) => handleMouseOver(index)}
                   >
@@ -147,52 +150,54 @@ function PdpCarousel(prodViewData: any) {
               })}
           </ul>
           {/* <button onClick={handleDownArrowClick}>Down arrow</button> */}
-         {isProductImageLessThanSix ? null : (
-          <div className="hidden lg:block cursor-pointer absolute bottom-[-13px] left-[36px]">
-            <img
+          {isProductImageLessThanSix ? null : (
+            <div className="hidden lg:block cursor-pointer absolute bottom-[-13px] left-[36px]">
+              <img
+                onClick={handleDownArrowClick}
+                src="images\carousel_down.png"
+                alt="image"
+                className={`m-auto ${
+                  prodResponse?.productImages?.value?.length <=
+                  selectedItemIndex
+                    ? "opacity-25"
+                    : ""
+                }`}
+              />
+            </div>
+          )}
+          {isProductImageLessThanSix ? null : (
+            <div
               onClick={handleDownArrowClick}
-              src="images\carousel_down.png"
-              alt="image"
-              className={`m-auto ${
-                lastIndex === prodResponse?.productImages?.value?.length
+              className={`carousel-next -right-6 lg:hidden cursor-pointer flex items-center absolute top-[27px] right-[-10px] ${
+                prodResponse?.productImages?.value?.length <= selectedItemIndex
                   ? "opacity-25"
                   : ""
               }`}
-            />
-          </div>
-         )} 
-        {isProductImageLessThanSix ? null : (
-          <div
-            onClick={handleDownArrowClick}
-            className={`carousel-next -right-6 lg:hidden cursor-pointer flex items-center absolute top-[27px] right-[-10px] ${
-              lastIndex === prodResponse?.productImages?.value?.length
-                ? "opacity-25"
-                : ""
-            }`}
-          >
-            <svg
-              className="focus:outline-none"
-              width="24"
-              height="49"
-              viewBox="0 0 48 49"
-              id="hcp-btn-006"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
             >
-              <circle cx="24" cy="24.8306" r="24" fill="#001A71"></circle>
-              <path
-                d="M20.9401 16.8306L19.0601 18.7106L25.1667 24.8306L19.0601 30.9506L20.9401 32.8306L28.9401 24.8306L20.9401 16.8306Z"
-                fill="#fff"
-              ></path>
-            </svg>
-          </div>
-        )}
+              <svg
+                className="focus:outline-none"
+                width="24"
+                height="49"
+                viewBox="0 0 48 49"
+                id="hcp-btn-006"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="24" cy="24.8306" r="24" fill="#001A71"></circle>
+                <path
+                  d="M20.9401 16.8306L19.0601 18.7106L25.1667 24.8306L19.0601 30.9506L20.9401 32.8306L28.9401 24.8306L20.9401 16.8306Z"
+                  fill="#fff"
+                ></path>
+              </svg>
+            </div>
+          )}
         </div>
         <div
           className={`lg:w-[526px] ${
             deviceWidth < 1024 && deviceWidth !== 0 ? "h-[300px]" : ""
           } box-border flex flex-row justify-center items-center p-2 bg-white rounded border border-solid border-mckblue lg:ml-14`}
         >
+          <p>{selectedItemIndex}</p>
           <ImageComponent
             src={
               (prodResponse?.productImages?.value &&
