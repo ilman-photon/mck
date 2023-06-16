745d493b862b3dec92ecec3c680c0153369bdc94
tamilvanan.b <tamilvanan.b@photon.com>
2023-06-16 13:40:23 +0000

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -19,33 +19,15 @@ const nextConfig = {
 
   env: {
     API_URL: dev
-      ? "https://mcco02mstrub73kprep.dxcloud.episerver.net"
+      ? "$(API_URL)"
       : test
-      ? "https://mcco02mstrub73kprep.dxcloud.episerver.net"
-      : "https://mcco02mstrub73kprep.dxcloud.episerver.net",
-      PASSWORD:"P@ssword123$",
-    USERNAME:"apiuser",
-    CLIENT_SECRET:"SuperSecretPassword",
-    MAPSAPIKEY:'AIzaSyCZy8PsqiP202lhDapwxE8r1qUgZtC-Vjk',
-    HEARTMAPAPIKEY:'180A0FF6-6659-44EA-8E03-2BE22C2B27A3',
-
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'mcco02mstrub73kprep.dxcloud.episerver.net',
-        port: '',
-        pathname: '/globalassets/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'mcco02mstrub73kprep.dxcloud.episerver.net',
-        port: '',
-        pathname: '/contentassets/**',
-      },
-      
-    ],
+      ? "https://mcco02mstrub73kinte.dxcloud.episerver.net"
+      : "https://mcco02mstrub73kinte.dxcloud.episerver.net",
+    PASSWORD:"$(PASSWORD)",
+    USERNAME:"$(USERNAME)",
+    CLIENT_SECRET:"$(CLIENT_SECRET)",
+    MAPSAPIKEY:'$(MAPSAPIKEY)',
+    HEARTMAPAPIKEY:'$(HEARTMAPAPIKEY)',
   },
 };
 
